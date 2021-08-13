ee33c04c3966645c71a940187a42a7c5e17fb7d6
Chaandha Raghav <chaandharaghav.cb19@bitsathy.ac.in>
2021-08-13 19:17:47 +0530

    Add sidebar with styles and toggle function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-require('./styles.scss');
+require("./styles.scss");
+
+const sidebarToggler = document.querySelector(".navbar-burger");
+const sidebar = document.querySelector("#sidebar");
+
+console.log(sidebarToggler, sidebar);
+
+sidebarToggler.addEventListener("click", function () {
+  this.classList.toggle("is-active");
+  sidebar.classList.toggle("is-hidden-mobile");
+});
