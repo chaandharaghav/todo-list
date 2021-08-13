8b6d1d3c624641679d7f8566a81de4747936a7c3
Chaandha Raghav <chaandharaghav.cb19@bitsathy.ac.in>
2021-08-13 19:58:03 +0530

    Add offset for tablets when sidebar is opened and offset permanently for desktops

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ require("./styles.scss");
 
 const sidebarToggler = document.querySelector(".navbar-burger");
 const sidebar = document.querySelector("#sidebar");
-
-console.log(sidebarToggler, sidebar);
+const mainContent = document.querySelector("#mainContent");
 
 sidebarToggler.addEventListener("click", function () {
   this.classList.toggle("is-active");
   sidebar.classList.toggle("is-hidden-mobile");
+
+  if (window.innerWidth > 750) {
+    mainContent.classList.toggle("offset");
+  }
 });
